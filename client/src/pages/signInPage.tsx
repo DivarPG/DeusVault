57ccae2ed4a7f9d70a55d52dc112b97f6a542d19
spee2gon <lhwtvcnhd@gmail.com>
2026-05-12 08:50:12 +0300

    feat(pages): добавлены страницы start, registration и signIn

---
+++ client/src/pages/signInPage.tsx
@@ -0,0 +1,16 @@
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+function SignInPage() {
+
+    return (
+        <>
+            <section id="center">
+                <footer>2026@DeusVault</footer>
+            </section>
+
+        </>
+    )
+}
+
+export default SignInPage
