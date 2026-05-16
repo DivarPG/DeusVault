fe65109abdb8d83c4663b461f9c492f3a8ac2bd9
Анастасия Бредихина <anastasiiyabredikhina@gmail.com>
2026-05-16 23:30:18 +0300

    refactor: email на username и добавлена валидация dto в соответствии с требованиями

--- server/src/auth/dto/login.dto.ts
+++ server/src/auth/dto/login.dto.ts
@@ -1,9 +1,17 @@
-import { IsString } from 'class-validator';
+import { IsString, Length, Matches } from 'class-validator';
 
 export class LoginDto {
   @IsString()
+  @Length(5, 10)
+  @Matches(/^[a-zA-Z]+$/, {
+    message: 'Username must contain only Latin letters',
+  })
   username: string;
 
   @IsString()
+  @Length(6, 10)
+  @Matches(/^[a-zA-Z0-9]+$/, {
+    message: 'Password must contain only Latin letters and digits',
+  })
   password: string;
 }
