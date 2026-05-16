e80f1418c199620dd03ebcf48ac268d532b845aa
Анастасия Бредихина <anastasiiyabredikhina@gmail.com>
2026-05-16 23:29:58 +0300

    feat: добавлены гварды к эндпоинтам коллекций и привязан доступ к пользователю

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { CollectionsModule } from './collections/collections.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, CollectionsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
